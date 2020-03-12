addee63a68d2d12e23559b3f4fab845ad0caf6b9
Dmuasya <muasyadennis4@gmail.com>
2020-03-12 16:08:21 +0300

    safisha

---
+++ app/src/main/java/com/dennis/usaficustomer/LaundryProfileActivity.java
@@ -0,0 +1,13 @@
+package com.dennis.usaficustomer;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class LaundryProfileActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_laundry_profile);
+    }
+}
---
+++ app/src/main/java/com/dennis/usaficustomer/Service/UserMyFirebaseMesseging.java
@@ -0,0 +1,14 @@
+package com.dennis.usaficustomer.Service;
+
+
+import com.google.firebase.messaging.FirebaseMessagingService;
+import com.google.firebase.messaging.RemoteMessage;
+
+
+public class UserMyFirebaseMesseging extends FirebaseMessagingService {
+    @Override
+    public void onMessageReceived(RemoteMessage remoteMessage) {
+
+
+    }
+}
